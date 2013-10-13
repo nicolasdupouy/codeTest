3a0615dcfc34b67ad9cb50afca3bf74a1d16c12c
Nicolas Dupouy <nicolasdupouy@gmail.com>
2013-10-13 21:56:50 +0200

    initial commit (Chapter 06)

---
+++ Android/Livre-SiteDuZeroAndroid/Chapter_06-Widgets/sdz-Chapter_06-Widgets - 2 - Boutton grossissant/src/com/example/bouttongrossissant/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.bouttongrossissant;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements View.OnTouchListener {
+
+	Button bouton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        bouton = (Button)findViewById(R.id.bouton);
+        bouton.setOnTouchListener(this);
+    }
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		bouton.setTextSize(Math.abs(event.getX() - bouton.getWidth()/2)
+						 + Math.abs(event.getY() - bouton.getHeight()/2));
+		return true;
+	}
+
+}

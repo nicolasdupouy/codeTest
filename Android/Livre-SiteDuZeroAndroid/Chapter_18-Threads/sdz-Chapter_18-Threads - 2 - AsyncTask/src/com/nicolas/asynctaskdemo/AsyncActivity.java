9d4d30417d23bc8737c4f53c5c0b0bce21d7612e
Nicolas Dupouy <nicolasdupouy@gmail.com>
2013-11-03 18:46:58 +0100

    Commit evolutions of project "sdz-Chapter_18-Threads - 2 - AsyncTask"

--- Android/Livre-SiteDuZeroAndroid/Chapter_18-Threads/sdz-Chapter_18-Threads - 2 - AsyncTask/src/com/nicolas/asynctaskdemo/AsyncActivity.java
+++ Android/Livre-SiteDuZeroAndroid/Chapter_18-Threads/sdz-Chapter_18-Threads - 2 - AsyncTask/src/com/nicolas/asynctaskdemo/AsyncActivity.java
@@ -1,13 +1,72 @@
 package com.nicolas.asynctaskdemo;
 
 import android.os.Bundle;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 
 public class AsyncActivity extends Activity {
 
+	private Button button = null;
+	private ProgressDialog progressBar = null;
+	private ProgressTask progressTask = null; 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_async);
+		
+		button = (Button)findViewById(R.id.button);
+		button.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				progressTask = new ProgressTask(AsyncActivity.this);
+				progressTask.execute();
+			}
+		});
+		
+		progressTask = (ProgressTask) getLastNonConfigurationInstance();
+		if (progressTask != null) {
+			progressTask.link(this);
+		}
+	}
+	
+	@Override
+	protected Dialog onCreateDialog(int id) {
+
+		//if (progressBar == null) {
+			progressBar = new ProgressDialog(this);
+
+			progressBar.setCancelable(true);
+			progressBar.setOnCancelListener(new DialogInterface.OnCancelListener() {
+
+				@Override
+				public void onCancel(DialogInterface dialog) {
+					progressTask.cancel(true);
+				}
+			});
+
+			progressBar.setTitle("Téléchargement en cours ...");
+			progressBar.setMessage("C'est long ...");
+			progressBar.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+			progressBar.setMax(ProgressTask.MAX_SIZE);
+		//}
+
+		return progressBar;
+	}
+
+
+	public void updateProgress(int progress) {
+		progressBar.setProgress(progress);
+	}
+	
+	public void dismiss() {
+		progressBar.dismiss();
 	}
 }
--- Android/Livre-SiteDuZeroAndroid/Chapter_18-Threads/sdz-Chapter_18-Threads - 2 - AsyncTask/src/com/nicolas/asynctaskdemo/ProgressTask.java
+++ Android/Livre-SiteDuZeroAndroid/Chapter_18-Threads/sdz-Chapter_18-Threads - 2 - AsyncTask/src/com/nicolas/asynctaskdemo/ProgressTask.java
@@ -1,36 +1,90 @@
 package com.nicolas.asynctaskdemo;
 
+import java.lang.ref.WeakReference;
+
 import android.os.AsyncTask;
+import android.widget.Toast;
 
 public class ProgressTask extends AsyncTask<Void, Integer, Boolean> {
 
+	private static final int PROGRESS_DIALOG_ID = 0;
+	public static final int MAX_SIZE = 10;
+	
+	private WeakReference<AsyncActivity> activityWeakReference = null;
+	private int progressionInt = 0;
+	
+	public ProgressTask(AsyncActivity asyncActivity) {
+		link(asyncActivity);
+	}
+	
 	@Override
 	protected void onPreExecute() {
-		// TODO Auto-generated method stub
-		super.onPreExecute();
+		if (activityWeakReference.get() != null) {
+			activityWeakReference.get().showDialog(PROGRESS_DIALOG_ID);
+		}
 	}
 
 	@Override
 	protected Boolean doInBackground(Void... params) {
-		// TODO Auto-generated method stub
-		return null;
+
+		try {
+			while (download() <= MAX_SIZE) {
+				publishProgress(progressionInt);
+				Thread.sleep(1000);
+			}
+			return true;
+		}
+		catch(InterruptedException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 
 	@Override
 	protected void onProgressUpdate(Integer... values) {
-		// TODO Auto-generated method stub
-		super.onProgressUpdate(values);
+		if (activityWeakReference.get() != null) {
+			activityWeakReference.get().updateProgress(values[0]);
+		}
 	}
 	
 	@Override
 	protected void onPostExecute(Boolean result) {
-		// TODO Auto-generated method stub
-		super.onPostExecute(result);
+		
+		if (activityWeakReference.get() != null) {
+			String text;
+			if (result) {
+				text = "Téléchargement terminé";
+			}
+			else {
+				text = "Echec du téléchargement";
+			}
+			
+			activityWeakReference.get().dismiss();
+			Toast.makeText(activityWeakReference.get(), 
+					text,
+					Toast.LENGTH_SHORT)
+					.show();
+		}
 	}
 	
 	@Override
 	protected void onCancelled() {
-		// TODO Auto-generated method stub
-		super.onCancelled();
+		if (activityWeakReference.get() != null) {
+			Toast.makeText(activityWeakReference.get(), 
+					R.string.cancelled, // ProgressBarWithHandlerDemo.this.getString(R.string.cancelled) 
+					Toast.LENGTH_SHORT)
+					.show();
+		}
+	}
+	
+	public void link (AsyncActivity asyncActivity) {
+		activityWeakReference = new WeakReference<AsyncActivity>(asyncActivity);
+	}
+	
+	private int download() {
+		if (progressionInt <= MAX_SIZE) {
+			return ++progressionInt;
+		}
+		return progressionInt; //MAX_SIZE;
 	}
 }

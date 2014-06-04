88c7603c56f9340fd7523fc80048ea0ea2c5fdb4
Nicolas Dupouy (MacBook Pro) <nicolasdupouy@gmail.com>
2014-06-04 08:08:55 +0200

    Coursera-POSA - Week 3 - commit assignment

--- Android/Coursera-CoursPOSADougSchmidt/Week3/week-3-assignment-2/src/edu/vuum/mocca/SimpleSemaphore.java
+++ Android/Coursera-CoursPOSADougSchmidt/Week3/week-3-assignment-2/src/edu/vuum/mocca/SimpleSemaphore.java
@@ -1,8 +1,7 @@
 package edu.vuum.mocca;
 
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @class SimpleSemaphore
@@ -14,57 +13,94 @@ import java.util.concurrent.locks.Condition;
  *        just liked Java Semaphores.
  */
 public class SimpleSemaphore {
-    /**
-     * Define a ReentrantLock to protect the critical section.
-     */
-    // TODO - you fill in here
+	/**
+	 * Define a ReentrantLock to protect the critical section.
+	 */
+	// TODO - you fill in here
+	ReentrantLock mReentrantLock;
 
-    /**
-     * Define a Condition that waits while the number of permits is 0.
-     */
-    // TODO - you fill in here
+	/**
+	 * Define a Condition that waits while the number of permits is 0.
+	 */
+	// TODO - you fill in here
+	Condition zeroPermitsNumberCondition;
 
-    /**
-     * Define a count of the number of available permits.
-     */
-    // TODO - you fill in here.  Make sure that this data member will
-    // ensure its values aren't cached by multiple Threads..
+	/**
+	 * Define a count of the number of available permits.
+	 */
+	// TODO - you fill in here. Make sure that this data member will
+	// ensure its values aren't cached by multiple Threads..
+	private int permitsNumber;
 
-    public SimpleSemaphore(int permits, boolean fair) {
-        // TODO - you fill in here to initialize the SimpleSemaphore,
-        // making sure to allow both fair and non-fair Semaphore
-        // semantics.
-    }
+	public SimpleSemaphore(int permits, boolean fair) {
+		// TODO - you fill in here to initialize the SimpleSemaphore,
+		// making sure to allow both fair and non-fair Semaphore
+		// semantics.
+		this.permitsNumber = permits;
 
-    /**
-     * Acquire one permit from the semaphore in a manner that can be
-     * interrupted.
-     */
-    public void acquire() throws InterruptedException {
-        // TODO - you fill in here.
-    }
+		mReentrantLock = new ReentrantLock(fair);
+		zeroPermitsNumberCondition = mReentrantLock.newCondition();
+	}
 
-    /**
-     * Acquire one permit from the semaphore in a manner that cannot be
-     * interrupted.
-     */
-    public void acquireUninterruptibly() {
-        // TODO - you fill in here.
-    }
+	/**
+	 * Acquire one permit from the semaphore in a manner that can be
+	 * interrupted.
+	 */
+	public void acquire() throws InterruptedException {
+		// TODO - you fill in here.
+		mReentrantLock.lock();
+		try {
+			if (permitsNumber == 0) {
+				zeroPermitsNumberCondition.await();
+			}
+			permitsNumber--;
+		} finally {
+			mReentrantLock.unlock();
+		}
+	}
 
-    /**
-     * Return one permit to the semaphore.
-     */
-    void release() {
-        // TODO - you fill in here.
-    }
+	/**
+	 * Acquire one permit from the semaphore in a manner that cannot be
+	 * interrupted.
+	 */
+	public void acquireUninterruptibly() {
+		// TODO - you fill in here.
+		mReentrantLock.lock();
+		try {
+			if (permitsNumber == 0) {
+				zeroPermitsNumberCondition.awaitUninterruptibly();
+			}
+			permitsNumber--;
+		} finally {
+			mReentrantLock.unlock();
+		}
+	}
 
-    /**
-     * Return the number of permits available.
-     */
-    public int availablePermits() {
-        // TODO - you fill in here by changing null to the appropriate
-        // return value.
-        return null;
-    }
+	/**
+	 * Return one permit to the semaphore.
+	 */
+	void release() {
+		// TODO - you fill in here.
+		mReentrantLock.lock();
+		try {
+			permitsNumber++;
+			zeroPermitsNumberCondition.signal();
+		} finally {
+			mReentrantLock.unlock();
+		}
+	}
+
+	/**
+	 * Return the number of permits available.
+	 */
+	public int availablePermits() {
+		// TODO - you fill in here by changing null to the appropriate
+		// return value.
+		mReentrantLock.lock();
+		try {
+			return permitsNumber;
+		} finally {
+			mReentrantLock.unlock();
+		}
+	}
 }
--- Android/Coursera-CoursPOSADougSchmidt/Week3/week-3-assignment-2/test/edu/vuum/mocca/PalantirManagerUnitTest.java
+++ Android/Coursera-CoursPOSADougSchmidt/Week3/week-3-assignment-2/test/edu/vuum/mocca/PalantirManagerUnitTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 
 /**
  * @class PalantirManagerUnitTest
- *
+ * 
  * @brief This program tests that student implementations of
  *        SimpleAtomicLong and SimpleSemaphore correctly implement a
  *        resource manager that limits the number of Beings from
@@ -20,227 +20,228 @@ import org.junit.Test;
  *        ;-)).
  */
 public class PalantirManagerUnitTest {
-    /**
-     * If this is set to true in then lots of debugging output will be
-     * generated.
-     */
-    public static boolean diagnosticsEnabled = false;
+	/**
+	 * If this is set to true in then lots of debugging output will be
+	 * generated.
+	 */
+	public static boolean diagnosticsEnabled = true;
 
-    /**
-     * Total number of times each Palantir user gets to gaze into a
-     * Palantir.
-     */
-    final static int mMaxPalantirSessions = 5;
+	/**
+	 * Total number of times each Palantir user gets to gaze into a
+	 * Palantir.
+	 */
+	final static int mMaxPalantirSessions = 5;
 
-    /**
-     * Total number of active Threads.
-     */
-    static volatile long mMaxActiveThreads = 0;
+	/**
+	 * Total number of active Threads.
+	 */
+	static volatile long mMaxActiveThreads = 0;
 
-    /**
-     * Count of the number of Active Threads.
-     */
-    static SimpleAtomicLong mActiveThreads = new SimpleAtomicLong(0);
+	/**
+	 * Count of the number of Active Threads.
+	 */
+	static SimpleAtomicLong mActiveThreads = new SimpleAtomicLong(0);
 
-    /**
-     * Resource Manager that controls access to the available
-     * Palantiri.
-     */
-    static PalantirManager mPalantirManager = null;
+	/**
+	 * Resource Manager that controls access to the available
+	 * Palantiri.
+	 */
+	static PalantirManager mPalantirManager = null;
 
-    /**
-     * Object that attempts to check whether the Semaphore
-     * implementation is "fair".
-     */
-    static FairnessChecker mFairnessChecker = null;
+	/**
+	 * Object that attempts to check whether the Semaphore
+	 * implementation is "fair".
+	 */
+	static FairnessChecker mFairnessChecker = null;
 
-    /**
-     * Runnable passed to each Thread that uses a Palantir. 
-     */
-    static Runnable usePalantir = new Runnable() {
-            /**
-             * This is the main loop run by each Being of Middle-Earth
-             * who wants to gaze into a Palantir.
-             */
-            public void run() {
-                // Bound the total number of times that each user can
-                // gaze into a Palantir.
-                for (int i = 0; i < mMaxPalantirSessions; ++i) {
-                    if (diagnosticsEnabled)
-                        System.out.println(Thread.currentThread().getName()
-                                           + " is acquiring the palantir");
+	/**
+	 * Runnable passed to each Thread that uses a Palantir.
+	 */
+	static Runnable usePalantir = new Runnable() {
+		/**
+		 * This is the main loop run by each Being of Middle-Earth
+		 * who wants to gaze into a Palantir.
+		 */
+		public void run() {
+			// Bound the total number of times that each user can
+			// gaze into a Palantir.
+			for (int i = 0; i < mMaxPalantirSessions; ++i) {
+				if (diagnosticsEnabled)
+					System.out.println(Thread.currentThread().getName()
+							+ " is acquiring the palantir");
 
-                    // Used to check for Semaphore fairness.
-                    mFairnessChecker.addNewThread(Thread.currentThread().getName());
+				// Used to check for Semaphore fairness.
+				mFairnessChecker.addNewThread(Thread.currentThread().getName());
 
-                    // Get access to a Palantir, which will block if
-                    // all the available Palantiri are in use.
-                    Palantir palantir = mPalantirManager.acquirePalantir();
+				// Get access to a Palantir, which will block if
+				// all the available Palantiri are in use.
+				Palantir palantir = mPalantirManager.acquirePalantir();
 
-                    // There's a race condition here since it's
-                    // possible for one thread to call
-                    // mFairnessChecker.addNewThread() and then yield
-                    // to another thread which again calls
-                    // mFairnessChecker.addNewThread() and then goes
-                    // on without interruption to call
-                    // mPalantirManager.acquirePalantir(), which will
-                    // fool the fairness checker into wrongly thinking
-                    // the acquisition wasn't fair. we'll just give a
-                    // warning (rather than an error) if it looks like
-                    // the semaphore acquire() method isn't "fair".
-                    if (!mFairnessChecker.checkOrder(Thread.currentThread()
-                                                     .getName())) {
-                        if (diagnosticsEnabled)
-                            System.out.println(Thread.currentThread().getName()
-                                               + ": warning, semaphore acquire may not be fair");
-                    }
+				// There's a race condition here since it's
+				// possible for one thread to call
+				// mFairnessChecker.addNewThread() and then yield
+				// to another thread which again calls
+				// mFairnessChecker.addNewThread() and then goes
+				// on without interruption to call
+				// mPalantirManager.acquirePalantir(), which will
+				// fool the fairness checker into wrongly thinking
+				// the acquisition wasn't fair. we'll just give a
+				// warning (rather than an error) if it looks like
+				// the semaphore acquire() method isn't "fair".
+				if (!mFairnessChecker.checkOrder(Thread.currentThread()
+						.getName())) {
+					if (diagnosticsEnabled)
+						System.out.println(Thread.currentThread().getName()
+								+ ": warning, semaphore acquire may not be fair");
+				}
 
-                    // Ensure that the Semaphore implementation is
-                    // correctly limiting the number of Palantir
-                    // gazers.
-                    long activeThreads = mActiveThreads.getAndIncrement();
-                    if (mMaxActiveThreads < activeThreads) {
-                        System.out.println("too many threads = " 
-                                           + activeThreads);
-                        throw new RuntimeException();
-                    }
-                    if (diagnosticsEnabled)
-                        System.out.println(Thread.currentThread().getName()
-                                           + " is starting to gaze at the " 
-                                           + palantir.name()
-                                           + " palantir");
+				// Ensure that the Semaphore implementation is
+				// correctly limiting the number of Palantir
+				// gazers.
+				long activeThreads = mActiveThreads.getAndIncrement();
+				if (mMaxActiveThreads < activeThreads) {
+					System.out.println("too many threads = "
+							+ activeThreads);
+					throw new RuntimeException();
+				}
+				if (diagnosticsEnabled)
+					System.out.println(Thread.currentThread().getName()
+							+ " is starting to gaze at the "
+							+ palantir.name()
+							+ " palantir");
 
-                    // Gaze at the Palantir for the time alloted in
-                    // the command.
-                    palantir.gaze();
+				// Gaze at the Palantir for the time alloted in
+				// the command.
+				palantir.gaze();
 
-                    // Indicate this Being is no longer using the
-                    // Palantir.
-                    mActiveThreads.decrementAndGet();
+				// Indicate this Being is no longer using the
+				// Palantir.
+				mActiveThreads.decrementAndGet();
 
-                    if (diagnosticsEnabled)
-                        System.out.println(Thread.currentThread().getName()
-                                           + " is finished gazing at the " 
-                                           + palantir.name()
-                                           + " palantir");
+				if (diagnosticsEnabled)
+					System.out.println(Thread.currentThread().getName()
+							+ " is finished gazing at the "
+							+ palantir.name()
+							+ " palantir");
 
-                    // Return the Palantir back to the shared pool so
-                    // other Beings can gaze at it.
-                    mPalantirManager.releasePalantir(palantir);
+				// Return the Palantir back to the shared pool so
+				// other Beings can gaze at it.
+				mPalantirManager.releasePalantir(palantir);
 
-                    if (diagnosticsEnabled)
-                        System.out.println(Thread.currentThread().getName()
-                                           + " has released the "
-                                           + palantir.name() 
-                                           + " palantir");
-                }
+				if (diagnosticsEnabled)
+					System.out.println(Thread.currentThread().getName()
+							+ " has released the "
+							+ palantir.name()
+							+ " palantir");
+			}
 
-            }
+		}
 	};
 
-    /**
-     * This factory creates a list of Palantiri.
-     */
-    static List<Palantir> makePalantiri() {
-        List<Palantir> palantiri = new ArrayList<Palantir>();
+	/**
+	 * This factory creates a list of Palantiri.
+	 */
+	static List<Palantir> makePalantiri() {
+		List<Palantir> palantiri = new ArrayList<Palantir>();
 
-        // MinasTirith Palantir
-        palantiri.add(new Palantir() {
-                public void gaze() {
-                    try {
-                        Thread.sleep(10);
-                    } catch (InterruptedException e) {
-                    }
-                }
-                public String name() {
-                    return "MinasTirith";
-                }
-            });
-        // Orthanc Palantir
-        palantiri.add(new Palantir() {
-                public void gaze() {
-                    try {
-                        Thread.sleep(150);
-                    } catch (InterruptedException e) {
-                    }
-                }
+		// MinasTirith Palantir
+		palantiri.add(new Palantir() {
+			public void gaze() {
+				try {
+					Thread.sleep(10);
+				} catch (InterruptedException e) {
+				}
+			}
 
-                public String name() {
-                    return "Orthanc";
-                }
-            });
-        // Barad-dur Palantir
-        palantiri.add(new Palantir() {
-                public void gaze() {
-                    try {
-                        // The unblinking eye gazes for a long time
-                        // ;-)
-                        Thread.sleep(100);
-                    } catch (InterruptedException e) {
-                    }
-                }
-                public String name() {
-                    return "Barad-dur";
-                }
-            });
+			public String name() {
+				return "MinasTirith";
+			}
+		});
+		// Orthanc Palantir
+		palantiri.add(new Palantir() {
+			public void gaze() {
+				try {
+					Thread.sleep(150);
+				} catch (InterruptedException e) {
+				}
+			}
 
-        return palantiri;
-    }
+			public String name() {
+				return "Orthanc";
+			}
+		});
+		// Barad-dur Palantir
+		palantiri.add(new Palantir() {
+			public void gaze() {
+				try {
+					// The unblinking eye gazes for a long time
+					// ;-)
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+				}
+			}
 
-    @Test
-    public void testPalantirManager() {
-        try {
-            if (diagnosticsEnabled)
-                System.out.println("Starting PalantirManagerTest");
+			public String name() {
+				return "Barad-dur";
+			}
+		});
 
-            // Get the list of available Palantiri.
-            List<Palantir> palantiri =
-                PalantirManagerUnitTest.makePalantiri();
+		return palantiri;
+	}
 
-            // Limit the number of users (threads) that can gaze into
-            // the available Palantiri.
-            mMaxActiveThreads = palantiri.size();
+	@Test
+	public void testPalantirManager() {
+		try {
+			if (diagnosticsEnabled)
+				System.out.println("Starting PalantirManagerTest");
 
-            // Create a resource manager that control access to the
-            // available Palantiri.
-            mPalantirManager = new PalantirManager(palantiri);
+			// Get the list of available Palantiri.
+			List<Palantir> palantiri =
+					PalantirManagerUnitTest.makePalantiri();
 
-            // Create a list of Middle-Earth Beings who want to use
-            // the Palantir.
-            List<Thread> palantirUsers = new ArrayList<Thread>();
-            palantirUsers.add(new Thread(usePalantir, "Pippen"));
-            palantirUsers.add(new Thread(usePalantir, "Aragorn"));
-            palantirUsers.add(new Thread(usePalantir, "Denathor"));
-            palantirUsers.add(new Thread(usePalantir, "Sauron"));
-            palantirUsers.add(new Thread(usePalantir, "Saruman"));
+			// Limit the number of users (threads) that can gaze into
+			// the available Palantiri.
+			mMaxActiveThreads = palantiri.size();
 
-            // Create an object that attempts to check whether the
-            // Semaphore implementation is "fair".
-            mFairnessChecker = new FairnessChecker(palantirUsers.size());
+			// Create a resource manager that control access to the
+			// available Palantiri.
+			mPalantirManager = new PalantirManager(palantiri);
 
-            // Start all the Threads that Middle-Earth Beings use to
-            // gaze into the Palantir.
-            for (ListIterator<Thread> iterator = palantirUsers.listIterator(); 
-                 iterator.hasNext();)
-                iterator.next().start();
+			// Create a list of Middle-Earth Beings who want to use
+			// the Palantir.
+			List<Thread> palantirUsers = new ArrayList<Thread>();
+			palantirUsers.add(new Thread(usePalantir, "Pippen"));
+			palantirUsers.add(new Thread(usePalantir, "Aragorn"));
+			palantirUsers.add(new Thread(usePalantir, "Denathor"));
+			palantirUsers.add(new Thread(usePalantir, "Sauron"));
+			palantirUsers.add(new Thread(usePalantir, "Saruman"));
 
-            // Barrier synchronization that waits for all the Threads
-            // to exit.
-            for (ListIterator<Thread> iterator = palantirUsers.listIterator(); iterator
-                     .hasNext();)
-                iterator.next().join();
+			// Create an object that attempts to check whether the
+			// Semaphore implementation is "fair".
+			mFairnessChecker = new FairnessChecker(palantirUsers.size());
 
-            if (diagnosticsEnabled)            
-                System.out.println("Finishing PalantirManagerTest");
-        } catch (Exception e) {
-            if (diagnosticsEnabled)
-        	System.out.println("A " 
-                                   + e.getMessage() 
-                                   + " Exception was thrown");
-            fail("A "
-                 + e.getMessage()
-                 + " Exception was thrown");
-        }
-    }
+			// Start all the Threads that Middle-Earth Beings use to
+			// gaze into the Palantir.
+			for (ListIterator<Thread> iterator = palantirUsers.listIterator(); iterator.hasNext();)
+				iterator.next().start();
+
+			// Barrier synchronization that waits for all the Threads
+			// to exit.
+			for (ListIterator<Thread> iterator = palantirUsers.listIterator(); iterator
+					.hasNext();)
+				iterator.next().join();
+
+			if (diagnosticsEnabled)
+				System.out.println("Finishing PalantirManagerTest");
+		} catch (Exception e) {
+			if (diagnosticsEnabled)
+				System.out.println("A "
+						+ e.getMessage()
+						+ " Exception was thrown");
+			fail("A "
+					+ e.getMessage()
+					+ " Exception was thrown");
+		}
+	}
 
 }

596bdc24583d17c43c9d698542a871316c8382db
Nicolas Dupouy (MacBook Pro) <nicolasdupouy@gmail.com>
2014-09-28 14:13:18 +0200

    Coursera-CloudServices - Weeks 9-10 - commit assignment.

--- Android/Coursera-CoursCloudServicesJulesWhite/Weeks 9-10 - Concurrent Webserver/Optional Assignment 4 - Concurrent Webserver (Java)/src/main/java/Program.java
+++ Android/Coursera-CoursCloudServicesJulesWhite/Weeks 9-10 - Concurrent Webserver/Optional Assignment 4 - Concurrent Webserver (Java)/src/main/java/Program.java
@@ -1,5 +1,4 @@
 import java.net.InetSocketAddress;
-import java.util.concurrent.Executors;
 
 import org.jboss.netty.bootstrap.ServerBootstrap;
 import org.jboss.netty.channel.Channel;
@@ -18,9 +17,11 @@ import org.jboss.netty.handler.codec.frame.DelimiterBasedFrameDecoder;
 import org.jboss.netty.handler.codec.frame.Delimiters;
 import org.jboss.netty.handler.codec.string.StringDecoder;
 import org.jboss.netty.handler.codec.string.StringEncoder;
-import org.jboss.netty.util.CharsetUtil;
+import org.jboss.netty.handler.execution.OrderedMemoryAwareThreadPoolExecutor;
 
 public class Program {
+
+	private static final Logger logger = new Logger();
 
 	public static void main(String[] args) throws Exception {
 		Program program = new Program();
@@ -48,25 +49,34 @@ public class Program {
 
 		private void initializePortNumber(String[] args) {
 			if (args == null || args.length == 0) {
-				System.err.println(ERROR_MISSING_PORT_NUMBER);
-				System.exit(-1);
+				exitOnError(ERROR_MISSING_PORT_NUMBER);
 			}
 			try {
 				portNumber = Integer.parseInt(args[0]);
 			} catch (NumberFormatException e) {
-				System.err.println(ERROR_BAD_FORMAT_PORT_NUMBER);
-				System.err.println("Given was: " + args[0]);
-				System.exit(-1);
+				exitOnError(ERROR_BAD_FORMAT_PORT_NUMBER
+						+ "\n" + "Given was: " + args[0]);
 			}
 		}
 
 		private void create() {
+			/* => [Half-Sync/Half-Async pattern]
+			 * 
+			 * Netty manages the pool of threads used for handling the multiple client connection requests.
+			 * As Netty provides a high level of abstraction. We don't have much work to do.
+			 * 
+			 * We create 2 thread pools
+			 * 1 - The Boss Threads: Threads provided by this pool are boss threads. Boss threads create and connect/bind sockets and then pass them off to the worker threads.
+			 *                       It the synchronous part of the Half-Sync/Half-Async pattern.
+			 * 2 - The Worker Threads: Worker threads perform all the asynchronous I/O
+			 *                       It the asynchronous part of the Half-Sync/Half-Async pattern.
+			 *                       
+			 */
 			ChannelFactory factory = new NioServerSocketChannelFactory(
-					Executors.newCachedThreadPool(),
-					Executors.newCachedThreadPool());
+					new OrderedMemoryAwareThreadPoolExecutor(4, 1048576, 1048576), //   Boss pool of threads
+					new OrderedMemoryAwareThreadPoolExecutor(16, 1048576, 1048576)); // Worker pool of threads
 
-			/*
-			 * => [Reactor pattern]
+			/* => [Reactor pattern]
 			 * 
 			 * It handles the requests made concurrently by multiples clients (connecting for exemple with "telnet localhost portNumber")
 			 * This is the reason of the pools of threads initialized before.
@@ -74,22 +84,19 @@ public class Program {
 			bootstrap = new ServerBootstrap(factory);
 			bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
 				public ChannelPipeline getPipeline() {
-					return Channels.pipeline(
-							new StringDecoder(CharsetUtil.UTF_8),
-							new StringEncoder(CharsetUtil.UTF_8),
-							/*
-							 * For information, the "Delimiters.lineDelimiter()" handles the ["chunk" at a time or (b) a "line" at a time] as professor Schmidt asked.
-							 * ==> http://docs.jboss.org/netty/3.2/api/org/jboss/netty/handler/codec/frame/Delimiters.html
-							 */
-							new DelimiterBasedFrameDecoder(ALLOWED_CHARACTER_BUFFER_SIZE, Delimiters.lineDelimiter()),
-							/*
-							 * => [Acceptor-Connector pattern] - Connector part (connect to the handler)
-							 * 
-							 * This is the service handler who manages the incoming requests and dispatches them synchronously to the associated request handlers
-							 * 
-							 * This part is the Connector part which dispatch to the request handler (which is here the "EchoServerHandler" class).
-							 */
-							new EchoServerHandler());
+					ChannelPipeline pipeline = Channels.pipeline();
+					pipeline.addLast("delimiter",
+							new DelimiterBasedFrameDecoder(ALLOWED_CHARACTER_BUFFER_SIZE, Delimiters.lineDelimiter()));
+					pipeline.addLast("decoder", new StringDecoder());
+					pipeline.addLast("encoder", new StringEncoder());
+					/* => [Acceptor-Connector pattern] - Connector part (connect to the handler)
+					 * 
+					 * This is the service handler who manages the incoming requests and dispatches them synchronously to the associated request handlers
+					 * This part is the Connector part which dispatch to the request handler (which is here the "EchoServerHandler" class).
+					 */
+					pipeline.addLast("handler", new EchoServerHandler());
+
+					return pipeline;
 				}
 			});
 
@@ -98,33 +105,37 @@ public class Program {
 		}
 
 		private void bind() {
-			/*
-			 * => [Acceptor-Connector pattern] - Acceptor part
+			/* => [Acceptor-Connector pattern] - Acceptor part
 			 * 
 			 * This is the service handler who manages the incoming requests and dispatches them synchronously to the associated request handlers
-			 * 
 			 * This part is the Acceptor part who listen to and accept the incoming requests. The hand will then be given to the Connector part (see previous comment).
 			 */
 			Channel acceptor = bootstrap.bind(new InetSocketAddress(portNumber));
 
 			if (!acceptor.isBound()) {
-				System.err.println("Server :: Error! Unable to bind to port " + portNumber);
-				System.exit(-1);
+				exitOnError("Server :: Error! Unable to bind to port " + portNumber);
 			}
 
-			System.out.println(
+			logger.info(
 					"Server :: Successfully bound to port "
 							+ portNumber + " !\n"
 							+ "Awaiting new connections...");
+		}
+
+		private void exitOnError(String errorMessage) {
+			logger.error(errorMessage);
+			System.exit(-1);
 		}
 	}
 
 	/*
 	 * => [Wrapper Facade pattern]
 	 * 
+	 * This pattern is symbolized in Netty by the classes/mechanisms "ChannelBuffer", "Channel", "Channels", etc... 
+	 * which manages the basic abstraction of the network. Hiding the underlying complexity.
+	 * 
 	 * It allows the developer to use a comfortable API (see the overrided methods below) instead of using directly 
 	 * sockets and complex data structure relationships as explain in the course.
-	 * In another words, it hides the complexity of the reality.
 	 */
 	public class EchoServerHandler extends SimpleChannelUpstreamHandler {
 
@@ -132,12 +143,14 @@ public class Program {
 		private static final String HEADER_SERVER_MESSAGE = "Server :: Received a new message from client";
 
 		private void printMessageToClient(MessageEvent e) {
-			e.getChannel().write(HEADER_CLIENT_MESSAGE
-					+ e.getMessage());
+			e.getChannel().write(
+					HEADER_CLIENT_MESSAGE
+							+ e.getMessage()
+							+ "\n");
 		}
 
 		private void printMessageToServer(MessageEvent e) {
-			System.out.println(HEADER_SERVER_MESSAGE
+			logger.info(HEADER_SERVER_MESSAGE
 					+ " ["
 					+ e.getChannel().getRemoteAddress() + "]\n"
 					+ "=> " + e.getMessage());
@@ -146,7 +159,7 @@ public class Program {
 		@Override
 		public void handleUpstream(ChannelHandlerContext ctx, ChannelEvent e) throws Exception {
 			if (e instanceof ChannelStateEvent) {
-				System.out.println("Server :: Channel state changed: " + e);
+				logger.info("Server :: Channel state changed: " + e);
 			}
 			super.handleUpstream(ctx, e);
 		}
@@ -154,8 +167,8 @@ public class Program {
 		@Override
 		public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) throws Exception {
 			e.getCause().printStackTrace();
-			System.err.println("Server :: exception caught " + e);
-			System.err.println("Server :: Channel will be closed !");
+			logger.error("Server :: exception caught " + e);
+			logger.error("Server :: Channel will be closed !");
 			e.getChannel().close();
 			super.exceptionCaught(ctx, e);
 		}
@@ -165,6 +178,16 @@ public class Program {
 			printMessageToClient(e);
 			printMessageToServer(e);
 			super.messageReceived(ctx, e);
+		}
+	}
+
+	private static class Logger {
+		public void info(String message) {
+			System.out.println(message);
+		}
+
+		public void error(String message) {
+			System.err.println(message);
 		}
 	}
 }

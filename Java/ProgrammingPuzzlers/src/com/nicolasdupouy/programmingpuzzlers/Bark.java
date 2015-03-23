af06245523e9b1461dd433c85f78d7441ceda52e
Nicolas Dupouy (iMac) <nicolasdupouy@gmail.com>
2015-03-23 23:15:53 +0100

    Create ProgrammingPuzzlers project from Joshua Bloch presentation to JavaOne 2002.

---
+++ Java/ProgrammingPuzzlers/src/com/nicolasdupouy/programmingpuzzlers/Bark.java
@@ -0,0 +1,23 @@
+package com.nicolasdupouy.programmingpuzzlers;
+
+class Dog {
+	public static void bark() {
+		System.out.println("woof ");
+	}
+}
+
+class Basenji extends Dog {
+	public static void bark() {
+		System.out.println("");
+	}
+}
+
+public class Bark {
+	public static void main(String[] args) {
+		Dog woofer = new Dog();
+		Dog nipper = new Basenji();
+		woofer.bark();
+		nipper.bark();
+		(new Basenji()).bark();
+	}
+}

3faa09089ed33996794bfc51ebf5b400926a4b2e
Nicolas Dupouy <nicolasdupouy@gmail.com>
2017-04-09 15:56:08 +0200

    Do the division using cargo

--- Rust/division/src/main.rs
+++ Rust/division/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let result = compute_division(-4, 2);
+    println!("Result: {}", result);
+}
+
+fn compute_division(x: i32, y: i32) -> i32 {
+    match y {
+        0 => panic!("Division by 0"),
+        1 => x,
+        _ => x/y
+    }
 }

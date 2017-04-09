a5184d06572483de152ddb2872c1ecd3f6edd242
Nicolas Dupouy <nicolasdupouy@gmail.com>
2017-04-09 16:03:18 +0200

    Read argument at command line

--- Rust/division/src/main.rs
+++ Rust/division/src/main.rs
@@ -1,4 +1,12 @@
+use std::env;
+
 fn main() {
+    let numerator = match env::args().nth(1) {
+        Some(argument) => argument,
+        None => panic!("Numerator mandatory in argument.")
+    };
+    println!("Numerator is: {}", numerator);
+
     let result = compute_division(-4, 2);
     println!("Result: {}", result);
 }

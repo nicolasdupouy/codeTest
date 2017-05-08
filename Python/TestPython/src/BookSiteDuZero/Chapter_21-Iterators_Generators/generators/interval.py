a89715e709ba34001d1a8e5a499566622b7369dc
Nicolas Dupouy (MacBook Pro) <nicolasdupouy@gmail.com>
2017-05-08 11:45:01 +0200

    Generators: Define and use an interval generator

---
+++ Python/TestPython/src/BookSiteDuZero/Chapter_21-Iterators_Generators/generators/interval.py
@@ -0,0 +1,5 @@
+
+def interval(lowerBound, upperBound):
+        while (lowerBound <= upperBound):
+            yield lowerBound
+            lowerBound += 1
---
+++ Python/TestPython/src/BookSiteDuZero/Chapter_21-Iterators_Generators/generators/testInterval.py
@@ -0,0 +1,6 @@
+#!/opt/bin/python3
+
+from interval import *
+
+for number in interval(1, 10):
+    print(number)

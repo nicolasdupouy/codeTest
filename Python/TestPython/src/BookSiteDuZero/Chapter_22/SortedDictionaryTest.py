ef8955a6b3bb0a0d645ce822f0b0af3967d51262
Nicolas Dupouy (MacBook Pro) <nicolasdupouy@gmail.com>
2017-05-26 22:06:54 +0200

    Define test for sort

--- Python/TestPython/src/BookSiteDuZero/Chapter_22/SortedDictionaryTest.py
+++ Python/TestPython/src/BookSiteDuZero/Chapter_22/SortedDictionaryTest.py
@@ -13,8 +13,12 @@ class SortedDictionaryTest(unittest.TestCase):
         self.fruits["strawberry"] = 128
         self.fruits["melon"] = 15
 
-    def test_SortedDictionary_should_be_represented(self):
+    def test_dictionary_should_be_represented(self):
         self.assertEqual("{'apple': 52, 'peach': 34, 'strawberry': 128, 'melon': 15}", repr(self.fruits))
+
+    def test_dictionary_should_be_sorted(self):
+        self.fruits.sort()
+        self.assertEqual("{'melon': 15, 'peach': 34, 'apple': 52, 'strawberry': 128}", str(self.fruits))
 
 if __name__ == '__main__':
     unittest.main()

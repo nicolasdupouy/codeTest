db067e5fb7767853dcc1bd9c75fd777b9120041d
Nicolas Dupouy (MacBook Pro) <nicolasdupouy@gmail.com>
2017-06-08 00:50:50 +0200

    Define the sort and the get (get is necessary for the sort)

--- Python/TestPython/src/BookSiteDuZero/Chapter_22/SortedDictionary.py
+++ Python/TestPython/src/BookSiteDuZero/Chapter_22/SortedDictionary.py
@@ -23,3 +23,19 @@ class SortedDictionary:
 
     def __str__(self):
         return repr(self)
+
+    def __getitem__(self, key):
+        if key not in self._keys:
+            raise KeyError("Unavailable key {0}".format(key))
+        else:
+            index = self._keys.index(key)
+            return self._values[index]
+
+    def sort(self):
+        sortedKeys = sorted(self._keys)
+        sortedValues = []
+        for key in sortedKeys:
+            sortedValues.append(self[key])
+
+        self._keys = sortedKeys
+        self._values = sortedValues

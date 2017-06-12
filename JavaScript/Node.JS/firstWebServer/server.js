1d05ae799f71279ad58ef221380c5e2e371f36e7
Nicolas Dupouy (MacBook Pro) <nicolasdupouy@gmail.com>
2017-06-12 19:52:28 +0200

    Use relative path for index.html

--- JavaScript/Node.JS/firstWebServer/server.js
+++ JavaScript/Node.JS/firstWebServer/server.js
@@ -4,7 +4,7 @@ let fs = require('fs');
 let server = http.createServer();
 server.on('request', (request, response) => {
     console.log("request received");
-    fs.readFile('/Users/nicolasdupouy/gitRepos/codeTest/JavaScript/Node.JS/firstWebServer/index.html', (err, data) => {
+    fs.readFile('index.html', (err, data) => {
         if (err) {
             response.writeHead(404);
             response.end("This file doesn't exists");

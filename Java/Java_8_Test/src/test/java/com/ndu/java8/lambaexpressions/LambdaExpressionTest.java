ebca0d4deaf08d691300a3ac2f2f0f17149d1976
Nicolas Dupouy (Debian SID @Edward) <nicolasdupouy@gmail.com>
2017-06-26 15:36:46 +0200

    Lambda expressions: old collection sort

---
+++ Java/Java_8_Test/src/test/java/com/ndu/java8/lambaexpressions/LambdaExpressionTest.java
@@ -0,0 +1,29 @@
+package com.ndu.java8.lambaexpressions;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class LambdaExpressionTest {
+
+    @Test
+    public void sort_list_with_older_method() {
+        // Given
+        List<String> names = Arrays.asList("B", "D", "C", "A");
+
+        // When
+        Collections.sort(names, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                return a.compareTo(b);
+            }
+        });
+
+        // Then
+        Assertions.assertEquals(names, Arrays.asList("A", "B", "C", "D"));
+    }
+}

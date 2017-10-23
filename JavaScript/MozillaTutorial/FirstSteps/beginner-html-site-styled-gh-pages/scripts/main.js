53a2cde5fefd8f261e8d6d80bed7350d8b084df3
Nicolas Dupouy <nicolasdupouy@gmail.com>
2017-10-23 19:46:55 +0200

    Switch logo and add functions

--- JavaScript/MozillaTutorial/FirstSteps/beginner-html-site-styled-gh-pages/scripts/main.js
+++ JavaScript/MozillaTutorial/FirstSteps/beginner-html-site-styled-gh-pages/scripts/main.js
@@ -1,2 +1,23 @@
+const LOGO_FIREFOX = 'images/firefox-icon.png';
+const LOGO_FREEBSD = 'images/FreeBSD-icon.png';
+
 let title = document.querySelector('h1');
-title.textContent = 'Hello Friend !';
+let image = document.querySelector('img');
+
+function changeTitle() {
+    title.textContent = 'Hello Friend !';
+}
+
+function switchImage() {
+    let imageSource = image.getAttribute('src');
+    
+    if (imageSource === LOGO_FIREFOX) {
+        image.setAttribute('src', LOGO_FREEBSD);
+    }
+    else {
+        image.setAttribute('src', LOGO_FIREFOX);
+    }
+}
+
+changeTitle();
+image.addEventListener('click', switchImage);

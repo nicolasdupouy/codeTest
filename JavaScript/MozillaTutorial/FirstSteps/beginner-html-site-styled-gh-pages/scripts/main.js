1d8accbbc64a0a2a04d47bda8fa2d2c7bdfeee97
Nicolas Dupouy <nicolasdupouy@gmail.com>
2017-10-23 20:00:04 +0200

    use localStorage to store the username

--- JavaScript/MozillaTutorial/FirstSteps/beginner-html-site-styled-gh-pages/scripts/main.js
+++ JavaScript/MozillaTutorial/FirstSteps/beginner-html-site-styled-gh-pages/scripts/main.js
@@ -1,23 +1,43 @@
 const LOGO_FIREFOX = 'images/firefox-icon.png';
 const LOGO_FREEBSD = 'images/FreeBSD-icon.png';
+const LOCALSTORAGE_USERNAME = 'userName';
 
 let title = document.querySelector('h1');
-let image = document.querySelector('img');
+let button = document.querySelector('button');
 
-function changeTitle() {
-    title.textContent = 'Hello Friend !';
+function initUserName() {
+    if (!localStorage.getItem(LOCALSTORAGE_USERNAME)) {
+        promptForUser();
+    }
+}
+
+function promptForUser() {
+    let userName = prompt('Please enter your name.');
+    localStorage.setItem(LOCALSTORAGE_USERNAME, userName);
+}
+
+function displayTitle() {
+    let registeredName = localStorage.getItem(LOCALSTORAGE_USERNAME);
+    title.textContent = title.textContent = 'Hello ' + registeredName;
 }
 
 function switchImage() {
-    let imageSource = image.getAttribute('src');
-    
-    if (imageSource === LOGO_FIREFOX) {
-        image.setAttribute('src', LOGO_FREEBSD);
-    }
-    else {
-        image.setAttribute('src', LOGO_FIREFOX);
+    let image = document.querySelector('img');
+    image.onclick = function() {
+        let imageSource = image.getAttribute('src');
+        if (imageSource === LOGO_FIREFOX) {
+            image.setAttribute('src', LOGO_FREEBSD);
+        }
+        else {
+            image.setAttribute('src', LOGO_FIREFOX);
+        }
     }
 }
 
-changeTitle();
-image.addEventListener('click', switchImage);
+initUserName();
+displayTitle();
+switchImage();
+button.onclick = function() {
+    promptForUser();
+    displayTitle();
+}

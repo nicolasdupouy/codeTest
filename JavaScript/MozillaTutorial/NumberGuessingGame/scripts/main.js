3f55d46fe5dc9f34a8046d3fb5885755fa905e91
Nicolas Dupouy <nicolasdupouy@gmail.com>
2017-10-24 01:26:50 +0200

    Fill previous propostions field

--- JavaScript/MozillaTutorial/NumberGuessingGame/scripts/main.js
+++ JavaScript/MozillaTutorial/NumberGuessingGame/scripts/main.js
@@ -9,3 +9,20 @@ var lowOrHi = document.querySelector('.lowOrHi');
 
 var guessCount = 1;
 var resetButton;
+
+function checkGuess() {
+    let userGuess = Number(guessField.value);
+    fillLastResults(userGuess);
+    console.log('lastResult = ' + lastResult.textContent);
+    
+    guessCount++;
+}
+
+function fillLastResults(userGuess) {
+    if (guessCount === 1) {
+        lastResult.textContent = 'Previous propositions :';
+    }
+    lastResult.textContent += ' ' + userGuess;
+}
+
+guessSubmit.addEventListener('click', checkGuess);

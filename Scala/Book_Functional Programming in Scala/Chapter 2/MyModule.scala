fb27550b6a98e2fc8cf89389cfb14ae3714afa8c
Nicolas Dupouy (Debian SID @Edward) <nicolasdupouy@gmail.com>
2017-12-31 22:12:37 +0100

    Add iterative Factorial

--- Scala/Book_Functional Programming in Scala/Chapter 2/MyModule.scala
+++ Scala/Book_Functional Programming in Scala/Chapter 2/MyModule.scala
@@ -1,14 +1,29 @@
 
 object MyModule {
   def abs(n: Int): Int =
-    if (n<0) -n
+    if (n < 0) -n
     else n
+
+  def factorial(n: Int): Int = {
+    def iter(n: Int, acc: Int): Int =
+      if (n <= 0) acc
+      else iter(n - 1, n * acc)
+
+    iter(n, 1)
+  }
 
   private def formatAbs(x: Int) = {
     val msg = "The absolute value of %d is %d"
     msg.format(x, abs(x))
   }
 
-  def main(args: Array[String]): Unit =
+  private def formatFactorial(x: Int): String = {
+    val msg = "The factorial of %d is %d"
+    msg.format(x, factorial(x))
+  }
+
+  def main(args: Array[String]): Unit = {
     println(formatAbs(-42))
+    println(formatFactorial(7))
+  }
 }

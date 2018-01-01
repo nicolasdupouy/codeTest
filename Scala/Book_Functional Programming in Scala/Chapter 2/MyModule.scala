5464aea9e3932fc1fc30b90c9cb5bffe5077c5d7
Nicolas Dupouy (Debian SID @Edward) <nicolasdupouy@gmail.com>
2018-01-02 00:15:35 +0100

    Exercice 2.1: Define a naive implementation of the Fibonacci number

--- Scala/Book_Functional Programming in Scala/Chapter 2/MyModule.scala
+++ Scala/Book_Functional Programming in Scala/Chapter 2/MyModule.scala
@@ -1,4 +1,3 @@
-
 object MyModule {
   def abs(n: Int): Int =
     if (n < 0) -n
@@ -13,6 +12,13 @@ object MyModule {
     iter(n, 1)
   }
 
+  private def fibonacci(n: Int): Int = {
+    def iteration(n: Int): Int =
+      if (n <= 1) n
+      else fibonacci(n-1) + fibonacci(n-2)
+    iteration(n)
+  }
+
   private def formatAbs(x: Int) = {
     val msg = "The absolute value of %d is %d"
     msg.format(x, abs(x))
@@ -23,8 +29,14 @@ object MyModule {
     msg.format(x, factorial(x))
   }
 
+  private def formatFibonacci(n: Int): String = {
+    val message = "The value of the %d Fibonacci number is %d"
+    message.format(n, fibonacci(n))
+  }
+
   def main(args: Array[String]): Unit = {
     println(formatAbs(-42))
     println(formatFactorial(7))
+    println(formatFibonacci(6))
   }
 }

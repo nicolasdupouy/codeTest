e2a4e120bc1504c6eec5c97f7c77b88f36d92d42
Nicolas Dupouy (Debian SID @Edward) <nicolasdupouy@gmail.com>
2018-03-26 23:24:54 +0200

    Exercice 2.4

--- Scala/Book_Functional Programming in Scala/Chapter 2/Exercices.scala
+++ Scala/Book_Functional Programming in Scala/Chapter 2/Exercices.scala
@@ -15,6 +15,10 @@ object Exercices {
     a => b => f(a, b)
   }
 
+  private def uncurry[A, B, C](f: A => B => C): (A, B) => C = {
+    (a, b) => f(a)(b)
+  }
+
   private def formatResult[T](as: Array[T], f: (T, T) => Boolean): String = {
     val message = "is sorted: %b"
     message.format(isSorted(as, f))
@@ -30,6 +34,7 @@ object Exercices {
     println(formatResult(Array(1, 4, 2, 3), (a: Int, b: Int) => a > b))
     println(formatResult(Array(4, 3, 2, 1, 0), (a: Int, b: Int) => a > b))
 
+    // Test curry
     def add(a: Int, b: Int): Int = a + b
     def addCurry = curry(add)
     def addCurried = (add _).curried
@@ -41,5 +46,14 @@ object Exercices {
     val b = addFour(10)
 
     println("add 5 to 5 = " + a + " / add 4 to 10 = " + b)
+
+    // Test uncurry
+    def addUncurry = uncurry(addCurry)
+    def addUncurried = uncurry(addCurried)
+    val c = addUncurry(1, 2)
+    val d = addUncurried(7, 8)
+
+
+    println("add 1 to 2 = " + c + "add 7 to 8 = " + d)
   }
 }

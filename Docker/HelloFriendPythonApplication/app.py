ee75ab8f44254ae1a6622c645f6be6c262f7f2de
Nicolas Dupouy <nicolasdupouy@gmail.com>
2018-04-10 19:52:38 +0200

    Update Docker image

--- Docker/HelloFriendPythonApplication/app.py
+++ Docker/HelloFriendPythonApplication/app.py
@@ -14,7 +14,7 @@ def hello():
     except RedisError:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
 
-    html = "<h3>Hello {name}i !</h3>" \
+    html = "<h3>Hello {name} !</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}"
     #print "Hello Friend !"
